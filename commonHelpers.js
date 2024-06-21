89c9b3b9ae5d2bf0b80f9dc0b5a4f03e523d1524
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2024-06-21 16:22:53 +0000

    Deploying to gh-pages from @ Karlson1979/project@3aa42e1102e6e10a6fd84815faaa77ac3156eea0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,23 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as p}from"./assets/vendor-bdb8a163.js";const i={formEl:document.querySelector(".js-hero-form"),heroContainer:document.querySelector(".js-hero-container")};i.formEl.addEventListener("submit",async r=>{r.preventDefault();const a=r.target.elements.query.value.trim();if(!a){console.error("Please enter a search term");return}const e=await c(a);e&&(i.heroContainer.innerHTML=h(e))});async function c(r){const t="https://superhero-search.p.rapidapi.com"+"/api/",o={"x-rapidapi-key":"abee44a7a8msh90b08298bfd61a9p194735jsn389487ef7d06","x-rapidapi-host":"superhero-search.p.rapidapi.com"},s=new URLSearchParams({hero:r});try{return(await p.request({url:t,params:s,headers:o})).data}catch(n){console.error(n)}}function h(r){const{appearance:a,biography:e,images:t,name:o,powerstats:s}=r;return`
+    <div class="hero-card card">
+      <div class="image-container">
+        <img src="${t.lg}" alt="${o}" class="hero-image" />
+      </div>
+      <div class="hero-body">
+        <h4 class="hero-name">${o}</h4>
+        <div class="hero-powerstats">
+          <p class="hero-bio">Full Name - ${e.fullName}</p>
+          <p class="hero-bio">Publisher - ${e.publisher}</p>
+          <p class="hero-bio">Alignment - ${e.alignment}</p>
+          <p class="hero-bio">Gender - ${a.gender}</p>
+          <p class="hero-bio">Race - ${a.race}</p>
+        </div>
+        <div class="hero-powerstats">
+          <span>Power: ${s.power}</span>
+          <span>Strength: ${s.strength}</span>
+          <span>Speed: ${s.speed}</span>
+          <span>Combat: ${s.combat}</span>
+        </div>
+      </div>
+    </div>`}
 //# sourceMappingURL=commonHelpers.js.map
